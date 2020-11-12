d8e6b53417f4c8b0e88f848fc80b068560bd52f6
javante_nelson <javantenelson@gmail.com>
2020-11-12 08:51:45 -0600

    add a nav bar for a links

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ import './App.css';
 function App() {
   return (
     
-      
-      <div className="alert alert-primary" role="alert">
-        <button type="button" className="btn btn-primary">Primary</button>
-  A simple primary alert—check it out!
+       <div> 
+     
+      <header>
+        <div className="logo"><h1>Javante Nelson |</h1></div>
+     
+   <nav>
+   <ul >
+  <li >
+    <a  href="#">Portfolio</a>
+  </li>
+  <li >
+    <a  href="#">Blog</a>
+  </li>
+  <li >
+    <a  href="#">Contact</a>
+  </li>
+  <li >
+    <a href="#" >About Me</a>
+  </li>
+</ul>
+</nav>
+</header>
 </div>
     
   );

7a08c03ba351d2efa4c999a9bd580672e9cd0294
javante_nelson <javantenelson@gmail.com>
2020-11-13 21:35:16 -0600

    style links and add card container for intro

--- src/App.js
+++ src/App.js
@@ -3,29 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    
        <div> 
-     
-      <header>
-        <div className="logo"><h1>Javante Nelson |</h1></div>
-     
-   <nav>
-   <ul >
-  <li >
-    <a  href="#">Portfolio</a>
-  </li>
-  <li >
-    <a  href="#">Blog</a>
-  </li>
-  <li >
-    <a  href="#">Contact</a>
-  </li>
-  <li >
-    <a href="#" >About Me</a>
-  </li>
-</ul>
-</nav>
-</header>
+          <nav>
+            <div class="logo">
+                <h4>Javante Nelson</h4>
+           </div>
+           <ul>
+             <li><a href="#">Home</a></li>
+             <li><a href="#">Portfolio</a></li>
+             <li><a href="#">Blog</a></li>
+             <li><a href="#">About Me</a></li>
+           </ul>
+         </nav>
+
+         <div className="container">
+             <div className="card">
+               <div className="lead">
+                 <p>coder</p>
+               </div>    
+             </div>
+         </div>
+
 </div>
     
   );
